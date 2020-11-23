70dcd8af769fe4426e4a3cfc0d84d85e088f2736
khusamov Albert <khusamov01@mail.ru>
2020-11-23 14:48:50 +0500

    v1.0

---
+++ Visualization-of-the-graph/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Visualization_of_the_graph
+{
+    partial class Main_Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main_Form));
+            this.Canvas_Panel = new System.Windows.Forms.Panel();
+            this.Clear_Button = new System.Windows.Forms.Button();
+            this.Instruct_Label = new System.Windows.Forms.Label();
+            this.Numb_TextBox = new System.Windows.Forms.TextBox();
+            this.Search_Button = new System.Windows.Forms.Button();
+            this.Result_TextBox = new System.Windows.Forms.TextBox();
+            this.Matrica_DataGridView = new System.Windows.Forms.DataGridView();
+            this.Create_Button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Matrica_DataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Canvas_Panel
+            // 
+            this.Canvas_Panel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.Canvas_Panel.BackColor = System.Drawing.Color.LightBlue;
+            this.Canvas_Panel.Location = new System.Drawing.Point(12, 12);
+            this.Canvas_Panel.Name = "Canvas_Panel";
+            this.Canvas_Panel.Size = new System.Drawing.Size(416, 397);
+            this.Canvas_Panel.TabIndex = 0;
+            this.Canvas_Panel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Canvas_Panel_MouseDown);
+            // 
+            // Clear_Button
+            // 
+            this.Clear_Button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Clear_Button.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.Clear_Button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Clear_Button.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.Clear_Button.Location = new System.Drawing.Point(148, 415);
+            this.Clear_Button.Name = "Clear_Button";
+            this.Clear_Button.Size = new System.Drawing.Size(127, 23);
+            this.Clear_Button.TabIndex = 1;
+            this.Clear_Button.Text = "Очистить";
+            this.Clear_Button.UseVisualStyleBackColor = false;
+            this.Clear_Button.Click += new System.EventHandler(this.Clear_Button_Click);
+            // 
+            // Instruct_Label
+            // 
+            this.Instruct_Label.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Instruct_Label.AutoSize = true;
+            this.Instruct_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Instruct_Label.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.Instruct_Label.Location = new System.Drawing.Point(511, 304);
+            this.Instruct_Label.Name = "Instruct_Label";
+            this.Instruct_Label.Size = new System.Drawing.Size(203, 16);
+            this.Instruct_Label.TabIndex = 3;
+            this.Instruct_Label.Text = "Введите начальную вершину:";
+            // 
+            // Numb_TextBox
+            // 
+            this.Numb_TextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Numb_TextBox.BackColor = System.Drawing.Color.AliceBlue;
+            this.Numb_TextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Numb_TextBox.ForeColor = System.Drawing.Color.DarkBlue;
+            this.Numb_TextBox.Location = new System.Drawing.Point(584, 323);
+            this.Numb_TextBox.Name = "Numb_TextBox";
+            this.Numb_TextBox.Size = new System.Drawing.Size(53, 20);
+            this.Numb_TextBox.TabIndex = 4;
+            // 
+            // Search_Button
+            // 
+            this.Search_Button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Search_Button.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.Search_Button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Search_Button.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.Search_Button.Location = new System.Drawing.Point(544, 349);
+            this.Search_Button.Name = "Search_Button";
+            this.Search_Button.Size = new System.Drawing.Size(127, 23);
+            this.Search_Button.TabIndex = 5;
+            this.Search_Button.Text = "Поиск в Ширину";
+            this.Search_Button.UseVisualStyleBackColor = false;
+            // 
+            // Result_TextBox
+            // 
+            this.Result_TextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Result_TextBox.BackColor = System.Drawing.Color.AliceBlue;
+            this.Result_TextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Result_TextBox.Enabled = false;
+            this.Result_TextBox.ForeColor = System.Drawing.Color.DarkBlue;
+            this.Result_TextBox.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.Result_TextBox.Location = new System.Drawing.Point(434, 389);
+            this.Result_TextBox.Name = "Result_TextBox";
+            this.Result_TextBox.ReadOnly = true;
+            this.Result_TextBox.Size = new System.Drawing.Size(354, 20);
+            this.Result_TextBox.TabIndex = 6;
+            // 
+            // Matrica_DataGridView
+            // 
+            this.Matrica_DataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Matrica_DataGridView.BackgroundColor = System.Drawing.Color.Lavender;
+            this.Matrica_DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Matrica_DataGridView.GridColor = System.Drawing.Color.DarkSlateBlue;
+            this.Matrica_DataGridView.Location = new System.Drawing.Point(434, 12);
+            this.Matrica_DataGridView.Name = "Matrica_DataGridView";
+            this.Matrica_DataGridView.Size = new System.Drawing.Size(354, 229);
+            this.Matrica_DataGridView.TabIndex = 7;
+            // 
+            // Create_Button
+            // 
+            this.Create_Button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Create_Button.BackColor = System.Drawing.Color.MediumPurple;
+            this.Create_Button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Create_Button.ForeColor = System.Drawing.Color.Indigo;
+            this.Create_Button.Location = new System.Drawing.Point(554, 247);
+            this.Create_Button.Name = "Create_Button";
+            this.Create_Button.Size = new System.Drawing.Size(127, 23);
+            this.Create_Button.TabIndex = 8;
+            this.Create_Button.Text = "Построить";
+            this.Create_Button.UseVisualStyleBackColor = false;
+            this.Create_Button.Click += new System.EventHandler(this.Create_Button_Click);
+            // 
+            // Main_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Create_Button);
+            this.Controls.Add(this.Matrica_DataGridView);
+            this.Controls.Add(this.Result_TextBox);
+            this.Controls.Add(this.Search_Button);
+            this.Controls.Add(this.Numb_TextBox);
+            this.Controls.Add(this.Instruct_Label);
+            this.Controls.Add(this.Clear_Button);
+            this.Controls.Add(this.Canvas_Panel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "Main_Form";
+            this.Text = "Лабораторная работа 4";
+            ((System.ComponentModel.ISupportInitialize)(this.Matrica_DataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Canvas_Panel;
+        private System.Windows.Forms.Button Clear_Button;
+        private System.Windows.Forms.Label Instruct_Label;
+        private System.Windows.Forms.TextBox Numb_TextBox;
+        private System.Windows.Forms.Button Search_Button;
+        private System.Windows.Forms.TextBox Result_TextBox;
+        private System.Windows.Forms.DataGridView Matrica_DataGridView;
+        private System.Windows.Forms.Button Create_Button;
+    }
+}
+
---
+++ Visualization-of-the-graph/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Visualization_of_the_graph
+{
+    public partial class Main_Form : Form
+    {
+        public Main_Form()
+        {
+            InitializeComponent();
+        }
+
+        struct Circle
+        {
+            public int x; 
+            public int y;
+            public int rad;
+        }
+
+        Circle [] koortoch = new Circle[50]; //массив точек
+        int n = 0; //счетчик текущей вершины
+        int versh = -1; //понадобиться нам для обработки различных ситуаций при нажатии мыши
+
+        private void Canvas_Panel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Right) // Если нажата правая кнопка мыши
+            {
+                Pen pen_default = new Pen(Color.Blue, 2);
+                koortoch[n].rad = 15;
+                koortoch[n].x = e.X - koortoch[n].rad;
+                koortoch[n].y = e.Y - koortoch[n].rad;
+                Canvas_Panel.CreateGraphics().DrawEllipse(pen_default, koortoch[n].x, koortoch[n].y, 
+                    koortoch[n].rad * 2, koortoch[n].rad * 2);
+                String drawString = (n+1).ToString();
+                Font drawFont = new Font("Arial", 14);
+                SolidBrush drawBrush = new SolidBrush(Color.Black);
+                PointF drawPoint = new PointF(koortoch[n].x + koortoch[n].rad/2, koortoch[n].y + koortoch[n].rad/2);
+                Canvas_Panel.CreateGraphics().DrawString(drawString, drawFont, drawBrush, drawPoint);
+                n++;
+            }
+            if (e.Button == MouseButtons.Left) // Если нажата левая кнопка мыши
+            {
+                Pen pen_selected = new Pen(Color.Red, 2);
+                if (versh == -1)//если пока не запомнили ни одну из вершин
+                {
+                    for (int i = 0; i < n; i++) //проверяем попал ли щелчок мыши на какую-то из вершин
+                        if (e.X - koortoch[i].rad <= koortoch[i].x + koortoch[i].rad &&
+                            e.X - koortoch[i].rad >= koortoch[i].x - koortoch[i].rad &&
+                            e.Y - koortoch[i].rad <= koortoch[i].y + koortoch[i].rad &&
+                            e.Y - koortoch[i].rad >= koortoch[i].y - koortoch[i].rad)
+                        {
+                            versh = i;//если попали, то запоминаем вершину по которой щелкнули
+                            Canvas_Panel.CreateGraphics().DrawEllipse(pen_selected, koortoch[i].x, koortoch[i].y,
+                    koortoch[i].rad * 2, koortoch[i].rad * 2);
+                            break;
+                        }
+                }
+                else //если по одной из вершин уже раннее щелкнули(запомнили), то
+                {
+                    int toversh = -1;
+                    for (int i = 0; i < n; i++) //проверяем попал ли щелчок мыши на какую-то из вершин
+                        if (e.X - koortoch[i].rad <= koortoch[i].x + koortoch[i].rad &&
+                            e.X - koortoch[i].rad >= koortoch[i].x - koortoch[i].rad &&
+                            e.Y - koortoch[i].rad <= koortoch[i].y + koortoch[i].rad &&
+                            e.Y - koortoch[i].rad >= koortoch[i].y - koortoch[i].rad)
+                        {
+                            toversh = i;//если попали, то запоминаем вершину по которой щелкнули
+                            Canvas_Panel.CreateGraphics().DrawEllipse(pen_selected, koortoch[i].x, koortoch[i].y,
+                    koortoch[i].rad * 2, koortoch[i].rad * 2);
+                            break;
+                        }
+                    if ((toversh != -1) && (versh != toversh))//если щелкнули сначала по одной, а потом по другой вершине
+                    {
+                        Point p1 = new Point(koortoch[versh].x + koortoch[versh].rad , koortoch[versh].y + koortoch[versh].rad );
+                        Point p2 = new Point(koortoch[toversh].x + koortoch[toversh].rad , koortoch[toversh].y + koortoch[toversh].rad );
+                        Canvas_Panel.CreateGraphics().DrawLine(pen_selected, p1, p2);
+                        toversh = -1;
+                        versh = -1;
+                    }
+                }
+            }
+            Result_TextBox.Text = versh.ToString();
+        }
+        
+
+
+        private void Clear_Button_Click(object sender, EventArgs e)
+        {
+            Canvas_Panel.Refresh();
+            n = 0;
+        }
+
+        private void Create_Button_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= n; ++i)
+            {
+                Matrica_DataGridView.Columns.Add(i.ToString(), i.ToString());
+                Matrica_DataGridView.Rows.Add();
+            }
+            for (int i = 1; i <= n; ++i)
+            {
+                Matrica_DataGridView[0, i].Value = i;
+                Matrica_DataGridView[i, 0].Value = i;
+            }
+        }
+    }
+}
